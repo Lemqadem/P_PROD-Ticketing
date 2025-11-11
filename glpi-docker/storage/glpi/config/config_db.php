3564b784c89972d6e9f8765ee71559a5f5b33fbc
Ervan Lemqadem <po53roe@eduvaud.ch>
2025-11-11 11:22:40 +0100

    feat docker container/image
    [45][Done]
    J'ai créer une image docker via un docker compose puis un container pour la db puis pour l'interfaceWEB. Je peux maintenant acceder à l'interface glpi via le local host

---
+++ glpi-docker/storage/glpi/config/config_db.php
@@ -0,0 +1,11 @@
+<?php
+class DB extends DBmysql {
+   public $dbhost = 'db:3306';
+   public $dbuser = 'glpi';
+   public $dbpassword = 'tw4M8kdMQ4b7676zZSMz';
+   public $dbdefault = 'db_ticketing';
+   public $use_timezones = true;
+   public $use_utf8mb4 = true;
+   public $allow_datetime = false;
+   public $allow_signed_keys = false;
+}
